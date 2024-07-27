592934306d749af64ae60b7dc163874d3b5b19d3
Ravi Dharigond <66740454+Ravidharigond1997@users.noreply.github.com>
2024-07-27 19:42:25 +0530

    its senced changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
